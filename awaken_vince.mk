f513d68f190612775a5a4f2ee1716c192fb71ae9
varundevonclite <varundevonclite@gmail.com>
2020-09-30 08:54:17 +0000

    vince: AwakenOS Bringup

--- awaken_vince.mk
+++ awaken_vince.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The RevengeOS Project
+# Copyright (C) 2020 The AwakenOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,21 +21,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common RevengeOS stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
+# Inherit some common awaken stuff.
+$(call inherit-product, vendor/awaken/config/common.mk)
+$(call inherit-product, vendor/awaken/config/gsm.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := revengeos_vince
+PRODUCT_NAME := awaken_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
-
-# Build Official
-REVENGEOS_BUILDTYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
